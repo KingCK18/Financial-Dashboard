337f7eba6d3ac5cac03a88a22e06c6aae0b90b6b
KingCK18 <christian.kirby2023@gmail.com>
2025-07-18 23:32:04 -0500

    Day 2: Header Component

--- components/header.jsx
+++ components/header.jsx
@@ -2,7 +2,7 @@ import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
 import { Button } from "./ui/button"
-import { LayoutDashboard } from "lucide-react"
+import { LayoutDashboard, PenBox } from "lucide-react"
 
 const Header = () => {
   return (
@@ -14,17 +14,26 @@ const Header = () => {
                 alt="Starfolio Logo"
                 height={100}
                 width={200}
-                className="h-12 w-auto object-contain"
+                className="h-16 w-auto object-contain"
                 />
             </Link>
-            <div>
+            
+            <div className="flex items-center space-x-4">
                 <SignedIn>
-                    <Link href={"/dashboard"}>
+                    <Link href={"/dashboard"} className="text-gray-600 hover:text-blue-600 flex items-center gap-2">
                         <Button variant="outline">
                             <LayoutDashboard size={18}/>
-                            <span>Dashboard</span>
+                            <span className="hidden md:inline">Dashboard</span>
                         </Button>
                     </Link>
+
+                    <Link href={"/transaction/create"}>
+                        <Button className="flex items-center gap-2">
+                            <PenBox size={18}/>
+                            <span className="hidden md:inline">Add Transaction</span>
+                        </Button>
+                    </Link>
+
                 </SignedIn>
                 <SignedOut>
                     <SignInButton forceRedirectUrl="/dashboard">
@@ -32,7 +41,13 @@ const Header = () => {
                     </SignInButton>
                 </SignedOut>
                 <SignedIn>
-                    <UserButton /> 
+                    <UserButton 
+                        appearance={{
+                            elements:{
+                                avatarBox: "w-12 h-12",
+                            },
+                        }} 
+                    /> 
                 </SignedIn>
             </div>
         </nav>
